667d7daf349748556b049e043b7c2a2403bfee87
VasylPopaz <pafos1997@gmail.com>
2024-11-22 17:39:46 +0100

    add fonts and container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    screens: {
+      "sm-max": { max: "374.98px" },
+      md: "768px",
+      lg: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        ".container": {
+          minWidth: "320px",
+          maxWidth: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          "@screen md": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            maxWidth: "1440px",
+          },
+        },
+      });
+    },
+  ],
 };
