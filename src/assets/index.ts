a088da2f906ef1671f833f3526b891a1182698b6
VasylPopaz <pafos1997@gmail.com>
2024-11-28 23:25:01 +0100

    add images for Home page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,62 @@
 import Icons from "./sprite.svg";
-export { Icons };
+// Panther
+import panther_mob1x from "./images/home/panther_mob1x.webp";
+import panther_mob2x from "./images/home/panther_mob2x.webp";
+import panther2_mob1x from "./images/home/panther2_mob1x.webp";
+import panther2_mob2x from "./images/home/panther2_mob2x.webp";
+
+import panther_tab1x from "./images/home/panther_tab1x.webp";
+import panther_tab2x from "./images/home/panther_tab2x.webp";
+import panther2_tab1x from "./images/home/panther2_tab1x.webp";
+import panther2_tab2x from "./images/home/panther2_tab2x.webp";
+
+import panther_desk1x from "./images/home/panther_desk1x.webp";
+import panther_desk2x from "./images/home/panther_desk2x.webp";
+import panther2_desk1x from "./images/home/panther2_desk1x.webp";
+import panther2_desk2x from "./images/home/panther2_desk2x.webp";
+
+// Spider-man
+import spiderMan_tab1x from "./images/home/spider-man_tab1x.webp";
+import spiderMan_tab2x from "./images/home/spider-man_tab2x.webp";
+import spiderMan2_tab1x from "./images/home/spider-man2_tab1x.webp";
+import spiderMan2_tab2x from "./images/home/spider-man2_tab2x.webp";
+
+import spiderMan2_desk1x from "./images/home/spider-man2_desk1x.webp";
+import spiderMan2_desk2x from "./images/home/spider-man2_desk2x.webp";
+
+// Hulk
+import hulk_tab1x from "./images/home/hulk_tab1x.webp";
+import hulk_tab2x from "./images/home/hulk_tab2x.webp";
+import hulk2_tab1x from "./images/home/hulk2_tab1x.webp";
+import hulk2_tab2x from "./images/home/hulk2_tab2x.webp";
+
+import hulk2_desk1x from "./images/home/hulk2_desk1x.webp";
+import hulk2_desk2x from "./images/home/hulk2_desk2x.webp";
+
+export {
+  Icons,
+  panther_mob1x,
+  panther_mob2x,
+  panther2_mob1x,
+  panther2_mob2x,
+  panther_tab1x,
+  panther_tab2x,
+  panther2_tab1x,
+  panther2_tab2x,
+  panther_desk1x,
+  panther_desk2x,
+  panther2_desk1x,
+  panther2_desk2x,
+  spiderMan2_tab1x,
+  spiderMan2_tab2x,
+  spiderMan_tab1x,
+  spiderMan_tab2x,
+  spiderMan2_desk1x,
+  spiderMan2_desk2x,
+  hulk_tab1x,
+  hulk_tab2x,
+  hulk2_tab1x,
+  hulk2_tab2x,
+  hulk2_desk1x,
+  hulk2_desk2x,
+};
