397a578a3636c9d2c44f9e65165e496c088772d9
VasylPopaz <pafos1997@gmail.com>
2024-12-14 23:33:30 +0100

    add characters images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -37,6 +37,60 @@ import hulk_desk2x from "./images/home/hulk_desk2x.webp";
 import hulk2_desk1x from "./images/home/hulk2_desk1x.webp";
 import hulk2_desk2x from "./images/home/hulk2_desk2x.webp";
 
+// Characters
+import ajak from "./images/characters/ajak.webp";
+import alexeiShostakov from "./images/characters/alexei-shostakov.webp";
+import americaChavez from "./images/characters/america-chavez.webp";
+import antMan from "./images/characters/ant-man.webp";
+import blackPanther from "./images/characters/black-panther.webp";
+import captainMarvel from "./images/characters/captain-marvel.webp";
+import cassieLang from "./images/characters/cassie-lang.webp";
+import collector from "./images/characters/the-collector.webp";
+import moonKnight from "./images/characters/moon-knight.webp";
+import deadpool from "./images/characters/deadpool.webp";
+import doctorOctopus from "./images/characters/doctor-octopus.webp";
+import doctorStrange from "./images/characters/doctor-strange.webp";
+import drax from "./images/characters/drax.webp";
+import electro from "./images/characters/electro.webp";
+import gamora from "./images/characters/gamora.webp";
+import gor from "./images/characters/gor.webp";
+import greenGoblin from "./images/characters/green-goblin.webp";
+import groot from "./images/characters/groot.webp";
+import hawkeye from "./images/characters/hawkeye.webp";
+import hela from "./images/characters/hela.webp";
+import hulk from "./images/characters/hulk.webp";
+import ironMan from "./images/characters/iron-man.webp";
+import kang from "./images/characters/kang.webp";
+import kariMordo from "./images/characters/kari-mordo.webp";
+import killmonger from "./images/characters/killmonger.webp";
+import lizard from "./images/characters/lizard.webp";
+import loki from "./images/characters/loki.webp";
+import mantis from "./images/characters/mantis.webp";
+import melinaVostokoff from "./images/characters/melina-vostokoff.webp";
+import natashaRomanoff from "./images/characters/natasha-romanoff.webp";
+import nebula from "./images/characters/nebula.webp";
+import nickFury from "./images/characters/nick-fury.webp";
+import rocket from "./images/characters/rocket.webp";
+import ronan from "./images/characters/ronan.webp";
+import sandman from "./images/characters/sandman.webp";
+import spiderMan from "./images/characters/spider-man.webp";
+import starLord from "./images/characters/star-lord.webp";
+import steveRogers from "./images/characters/steve-rogers.webp";
+import thanos from "./images/characters/thanos.webp";
+import thena from "./images/characters/thena.webp";
+import thor from "./images/characters/thor.webp";
+import valkiria from "./images/characters/valkiria.webp";
+import venom from "./images/characters/venom.webp";
+import vision from "./images/characters/vision.webp";
+import wandaMaximoff from "./images/characters/wanda-maximoff.webp";
+import wasp from "./images/characters/wasp.webp";
+import warMachine from "./images/characters/war-machine.webp";
+import winterSoldier from "./images/characters/winter-soldier.webp";
+import wolverine from "./images/characters/wolverine.webp";
+import wong from "./images/characters/wong.webp";
+import yelenaBelova from "./images/characters/yelena-belova.webp";
+import yondu from "./images/characters/yondu.webp";
+
 export {
   Icons,
   panther_mob1x,
@@ -67,4 +121,56 @@ export {
   hulk_desk2x,
   hulk2_desk1x,
   hulk2_desk2x,
+  ajak,
+  alexeiShostakov,
+  americaChavez,
+  antMan,
+  blackPanther,
+  captainMarvel,
+  cassieLang,
+  collector,
+  moonKnight,
+  deadpool,
+  doctorOctopus,
+  doctorStrange,
+  drax,
+  electro,
+  gamora,
+  gor,
+  greenGoblin,
+  groot,
+  hawkeye,
+  hela,
+  hulk,
+  ironMan,
+  kang,
+  kariMordo,
+  killmonger,
+  lizard,
+  loki,
+  mantis,
+  melinaVostokoff,
+  natashaRomanoff,
+  nebula,
+  nickFury,
+  rocket,
+  ronan,
+  sandman,
+  spiderMan,
+  starLord,
+  steveRogers,
+  thanos,
+  thena,
+  thor,
+  valkiria,
+  venom,
+  vision,
+  wandaMaximoff,
+  wasp,
+  warMachine,
+  winterSoldier,
+  wolverine,
+  wong,
+  yelenaBelova,
+  yondu,
 };
