02b93ddcf1cc779e8c1d374042bc24494cf9104d
VasylPopaz <pafos1997@gmail.com>
2024-12-16 13:09:04 +0100

    add axios, react-query, and react-toastify dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer, Zoom } from "react-toastify";
 
 import { App } from "./components";
 
 import "modern-normalize/modern-normalize.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ToastContainer
+        autoClose={3000}
+        transition={Zoom}
+        theme="dark"
+        closeOnClick
+      />
+    </QueryClientProvider>{" "}
   </BrowserRouter>
 );
