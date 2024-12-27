89f5888b0e56975951a7b1970598a4d25780e64d
VasylPopaz <pafos1997@gmail.com>
2024-12-27 20:08:27 +0100

    add Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,48 @@
+import ReactDOM from "react-dom";
+
+import { Icon } from "../index";
+
+interface ModalProps {
+  children: React.ReactNode;
+  toggleModal: () => void;
+  handleBackdropClick: (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => void;
+  modalRef: React.RefObject<HTMLDivElement>;
+}
+
+const modalRoot = document.querySelector("#modal-root")!;
+
+export const Modal = ({
+  children,
+  toggleModal,
+  handleBackdropClick,
+  modalRef,
+}: ModalProps) => {
+  return ReactDOM.createPortal(
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-[#0000004d] backdrop-blur-md"
+      onClick={(e) => handleBackdropClick(e)}
+    >
+      <div
+        ref={modalRef}
+        tabIndex={-1}
+        className="relative z-10 max-h-[98dvh] max-w-[335px] bg-primaryBgColor px-[20px] py-[40px] md:max-w-[600px] md:px-[32px] md:py-[50px] lg:max-w-[1000px]"
+      >
+        <button
+          type="button"
+          onClick={toggleModal}
+          className="absolute right-[16px] top-[16px] focus-visible:outline focus-visible:outline-primaryTextColor"
+          aria-label="Close modal"
+        >
+          <Icon
+            id="close"
+            className="size-[20px] stroke-primaryTextColor md:size-[28px]"
+          />
+        </button>
+        {children}
+      </div>
+    </div>,
+    modalRoot
+  );
+};
