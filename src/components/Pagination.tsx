e49b7ba29b96132f133650c06c2e12cb22fd998c
VasylPopaz <pafos1997@gmail.com>
2025-02-09 15:46:47 +0100

    Add Pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,50 @@
+import ReactPaginate from "react-paginate";
+
+import { Icon } from "../components";
+
+import { useBreakpointValue } from "../hooks";
+
+interface PaginationProps {
+  page: number;
+  totalPages: number;
+  handlePageChange: (page: number) => void;
+}
+
+export const Pagination = ({
+  page,
+  totalPages,
+  handlePageChange,
+}: PaginationProps) => {
+  const pagesDisplayed = useBreakpointValue([2, 2, 1]);
+
+  const handlePageClick = ({ selected }: { selected: number }): void => {
+    handlePageChange(selected + 1);
+  };
+
+  if (page > totalPages && totalPages <= 1) return;
+
+  return (
+    <ReactPaginate
+      forcePage={page ? page - 1 : 0}
+      onPageChange={handlePageClick}
+      marginPagesDisplayed={pagesDisplayed}
+      pageRangeDisplayed={1}
+      pageCount={totalPages}
+      breakLabel="..."
+      previousLabel={
+        <Icon id="chevron-left" className="size-[12px] fill-white" />
+      }
+      nextLabel={<Icon id="chevron-right" className="size-[12px] fill-white" />}
+      containerClassName=" flex justify-center items-center gap-[8px] text-[14px] font-medium leading-[1.29]"
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      activeClassName="active-pg"
+      previousClassName={"page-item"}
+      previousLinkClassName={"page-link  page-link-prev"}
+      nextClassName={"page-item"}
+      nextLinkClassName={"page-link page-link-next"}
+      breakClassName={"page-item"}
+      breakLinkClassName={"page-link"}
+    />
+  );
+};
