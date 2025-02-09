0ef0ae8d7234d4472b8a844cf774fe4bb13aed29
VasylPopaz <pafos1997@gmail.com>
2025-02-09 15:47:48 +0100

    Update message on fetch error

--- src/helpers/wrapErrorHandling.ts
+++ src/helpers/wrapErrorHandling.ts
@@ -8,7 +8,10 @@ export const wrapErrorHandling =
       return await callback(...args);
     } catch (err) {
       const error = err as AxiosError<{ message: string }>;
-      toast.error(error.response?.data.message || "An error occurred");
+      toast.error(
+        error.response?.data.message ||
+          "Sorry, something went wrong. Please try again."
+      );
       return null;
     }
   };
